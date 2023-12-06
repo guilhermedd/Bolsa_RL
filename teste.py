2c134c25b4acb3b6c2054b888704f5cfdfcc5dd0
guilherme diel <guiddiel@gmail.com>
2023-12-06 17:27:08 -0300

    added git ignore

--- teste.py
+++
@@ -1,8 +0,0 @@
-import os
-
-cwd = os.getcwd()
-print("cwd = ", cwd)
-# main.py
-with open(f"{cwd}/output.txt", "w") as f:
-    f.write("Hello, Docker!")
-    print(f"Arquivo criado em {cwd}/output.txt")
